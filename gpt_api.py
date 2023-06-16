de2cd3c40420b7c1bfa02a1a9b9183598a1ee38d
Mzzy <76478121+mzy8329@users.noreply.github.com>
2023-06-16 21:11:53 +0800

    Create gpt_api.py

---
+++ gpt_api.py
@@ -0,0 +1,21 @@
+import openai
+import os
+
+API_KEYS = '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'
+
+os.environ["HTTP_PROXY"] = "http://127.0.0.1:64835"
+os.environ["HTTPS_PROXY"] = "http://127.0.0.1:64835"
+
+openai.api_key = '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'
+
+
+def q2a(q):
+	rsp = openai.ChatCompletion.create(
+	model="gpt-3.5-turbo",
+		messages=[
+			{"role": "system", "content": "工程师"},
+			{"role": "user", "content": q}
+		]
+	)
+
+	return(rsp.get("choices")[0]["message"]["content"])
