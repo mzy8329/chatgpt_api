c401c0eda60a9ce58c47dfffc9e36d543069ebeb
Mzzy <76478121+mzy8329@users.noreply.github.com>
2023-06-16 21:15:19 +0800

    Update gpt_api.py

--- gpt_api.py
+++ gpt_api.py
@@ -1,12 +1,10 @@
 import openai
 import os
 
-API_KEYS = 'YOU API KEYS'
-
 os.environ["HTTP_PROXY"] = "http://127.0.0.1:64835"
 os.environ["HTTPS_PROXY"] = "http://127.0.0.1:64835"
 
-openai.api_key = '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'
+openai.api_key = 'YOU API KEYS'
 
 
 def q2a(q):
